7ab0e71d433ccc7ff41516ca87288783ca8a8931
khanzzirfan <khanzz_irfan@hotmail.com>
2020-07-16 10:47:49 +1000

    chore(config): updated config

--- codecept.webdriver.conf.js
+++ codecept.webdriver.conf.js
@@ -1,42 +1,47 @@
-const { setHeadlessWhen } = require('@codeceptjs/configure');
+const { setHeadlessWhen } = require("@codeceptjs/configure");
 
 setHeadlessWhen(process.env.HEADLESS);
 
 exports.config = {
-  tests: './todomvc-tests/**/*_test.js',
-  output: './output',
+  tests: "./todomvc-tests/**/*_tesdsdt.js",
+  output: "./output",
   helpers: {
     WebDriver: {
-      url: 'http://localhost',
-      browser: 'chrome',
+      url: "http://localhost",
+      browser: "chrome",
+      smartWait: 5000,
+      waitForTimeout: 20000,
+      timeouts: {
+        implicit: 5000,
+        script: 60000,
+        "page load": 10000,
+      },
     },
 
     REST: {},
 
     CustomHelper: {
-      require: './todomvc-tests/helpers/custom.helper.js'
-    }
+      require: "./todomvc-tests/helpers/custom.helper.js",
+    },
   },
 
   gherkin: {
-    features: './todomvc-tests/features/*.feature',
-    steps: [
-      './todomvc-tests/step-definitions/create-todos.steps.js'
-    ]
+    features: "./todomvc-tests/features/*.feature",
+    steps: ["./todomvc-tests/step-definitions/create-todos.steps.js"],
   },
 
   include: {
-    TodosPage: './todomvc-tests/pages/todos.page.js'
+    TodosPage: "./todomvc-tests/pages/todos.page.js",
   },
 
   plugins: {
     wdio: {
       enabled: true,
-      services: ['selenium-standalone']
-    }
-  },  
+      services: ["selenium-standalone"],
+    },
+  },
 
   bootstrap: null,
   mocha: {},
-  name: 'codecept demo tests'
-}
+  name: "codecept demo tests",
+};
